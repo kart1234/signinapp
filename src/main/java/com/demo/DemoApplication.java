7b58103eb1f9ba4df151d4bdcf96ca3c5087ccd9
Prajakta <chavanp.prajakta@gmail.com>
2015-11-26 01:51:55 -0500

    update to deploy in tomcat

--- src/main/java/com/demo/DemoApplication.java
+++ src/main/java/com/demo/DemoApplication.java
@@ -6,13 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 
 
 @SpringBootApplication
-public class DemoApplication {
-	
+public class DemoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoApplication.class);
+    }
     public static void main(String[] args) {
     	SpringApplication.run(DemoApplication.class, args);
     }
